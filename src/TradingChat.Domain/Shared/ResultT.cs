69d8179c1fa6751c5531954dabb46635f742ec74
Túlio Paim <tutpaim@gmail.com>
2023-03-03 20:46:28 -0300

    feat: shared result and error classes

---
+++ src/TradingChat.Domain/Shared/ResultT.cs
@@ -0,0 +1,31 @@
+﻿namespace TradingChat.Domain.Shared;
+
+public class Result<TValue> : Result
+{
+    private readonly TValue? _value;
+
+    protected internal Result(TValue? value, bool isSuccess)
+        : base(isSuccess)
+    {
+        _value = value;
+    }
+
+    protected internal Result(TValue? value, bool isSuccess, Error error)
+        : base(isSuccess, error)
+    {
+        _value = value;
+    }
+
+    protected internal Result(TValue? value, bool isSuccess, IEnumerable<Error> errors)
+        : base(isSuccess, errors)
+    {
+        _value = value;
+    }
+
+    public TValue Value => IsSuccess
+        ? _value!
+        : throw new InvalidOperationException("The value of a failure result can not be accessed.");
+
+    public static implicit operator Result<TValue>(TValue? value) => Success(value);
+    public static implicit operator Result<TValue>(Error error) => WithError<TValue>(error);
+}
