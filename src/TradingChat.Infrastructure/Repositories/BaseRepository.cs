b72b7c7b5a6786735327f5d6d906717a67f797bc
Túlio Paim <tutpaim@gmail.com>
2023-03-03 20:49:28 -0300

    feat: base repository

---
+++ src/TradingChat.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TradingChat.Domain.Contracts;
+using TradingChat.Infrastructure.Context;
+
+namespace TradingChat.Infrastructure.Repositories;
+
+public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+{
+    private readonly TradingChatDbContext _context;
+
+    protected BaseRepository(TradingChatDbContext context)
+    {
+        _context = context;
+    }
+
+    public IQueryable<TEntity> Get() => _context.Set<TEntity>().AsTracking();
+
+    public IQueryable<TEntity> GetAsNoTracking() => Get().AsNoTrackingWithIdentityResolution();
+
+    public void Add(TEntity entity) => _context.Add(entity);
+
+    public void Remove(TEntity entity) => _context.Remove(entity);
+
+    public Task SaveChangesAsync(CancellationToken cancellationToken) => _context.SaveChangesAsync(cancellationToken);
+}
