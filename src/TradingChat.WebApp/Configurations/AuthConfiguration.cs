d82d24550fe4af483455fb70592cee8018c4d80e
Túlio Paim <tutpaim@gmail.com>
2023-03-03 00:39:20 -0300

    feat: auth configuration with cookie rename

---
+++ src/TradingChat.WebApp/Configurations/AuthConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using TradingChat.Infrastructure.Persistence;
+
+namespace TradingChat.WebApp.Configurations;
+
+public static class AuthConfiguration
+{
+    public static IServiceCollection AddAuth(
+        this IServiceCollection services)
+    {
+        services.AddIdentity();
+
+        services.Configure<CookieAuthenticationOptions>(
+            IdentityConstants.ApplicationScheme, options =>
+            {
+                options.Cookie.Name = "TradingClientAuth";
+                options.LoginPath = "/login";
+                options.LogoutPath = "/logout";
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+                options.SlidingExpiration = true;
+            });
+
+        return services;
+    }
+
+    private static IServiceCollection AddIdentity(
+        this IServiceCollection services)
+    {
+        services.AddIdentity<IdentityUser<Guid>, IdentityRole<Guid>>(config =>
+        {
+            config.User.RequireUniqueEmail = false;
+            config.Password.RequiredLength = 6;
+            config.Password.RequireDigit = false;
+            config.Password.RequireUppercase = false;
+            config.Password.RequireLowercase = false;
+            config.Password.RequireNonAlphanumeric = false;
+            config.SignIn.RequireConfirmedEmail = false;
+        })
+        .AddEntityFrameworkStores<TradingChatDbContext>()
+        .AddDefaultTokenProviders();
+
+        return services;
+    }
+
+
+}
+
