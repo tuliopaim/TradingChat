c567dbb00a0ee849bbac910671ae8bcde8ecdc03
Túlio Paim <tutpaim@gmail.com>
2023-03-04 16:50:33 -0300

    feat: send message use case

---
+++ src/TradingChat.Application/UseCases/SendMessage/SendMessageCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace TradingChat.Application.UseCases.SendMessage;
+
+public class SendMessageCommandValidator : AbstractValidator<SendMessageCommand>
+{
+    public SendMessageCommandValidator()
+    {
+        RuleFor(x => x.ChatRoomId).NotEmpty();
+
+        RuleFor(x => x.Message)
+            .NotEmpty()
+            .Length(1, 1000);
+    }
+}
