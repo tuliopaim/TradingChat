fe4d8349d3fb549c12f4d55a42e274372404d474
Túlio Paim <tutpaim@gmail.com>
2023-03-03 22:37:13 -0300

    feat: views for create and list the created chats

--- src/TradingChat.WebApp/Controllers/ChatController.cs
+++ src/TradingChat.WebApp/Controllers/ChatController.cs
@@ -1,13 +1,55 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using TradingChat.Domain.UseCases.CreateChatRoom;
+using TradingChat.Domain.UseCases.GetChatsInfo;
+using TradingChat.WebApp.ViewModels;
 
 namespace TradingChat.WebApp.Controllers;
 
 public class ChatController : Controller
 {
+    [HttpGet]
     [Authorize]
-    public IActionResult Index()
+    public async Task<IActionResult> Index(
+        [FromServices] IMediator mediator,
+        CancellationToken cancellationToken)
     {
+        var chatsInfo = await mediator.Send(new GetChatsInfoQuery(), cancellationToken);
+
+        return View(chatsInfo.Value);
+    }
+
+    [HttpGet]
+    [Authorize]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [Authorize]
+    public async Task<IActionResult> Create(
+        [FromServices] IMediator mediator,
+        CreateChatRoomModel model,
+        CancellationToken cancellationToken)
+    {
+        if (!ModelState.IsValid) return View();
+
+        var command = new CreateChatRoomCommand(model.Name!, model.MaxNumberOfUsers);
+
+        var result = await mediator.Send(command, cancellationToken);
+
+        if (result.IsSuccess)
+        {
+            return RedirectToAction("Index");
+        }
+
+        foreach(var error in  result.Errors)
+        {
+            ModelState.AddModelError("", error.Message);
+        }
+        
         return View();
     }
 }
