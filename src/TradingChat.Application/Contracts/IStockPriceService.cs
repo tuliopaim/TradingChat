726977c4029e828040a6b40ea4c33903abd2c6db
Túlio Paim <tutpaim@gmail.com>
2023-03-05 14:40:57 -0300

    refactor: remove the GetStockPriceComand from application

--- src/TradingChat.Application/Contracts/IStockPriceService.cs
+++
@@ -1,9 +0,0 @@
-﻿using TradingChat.Application.UseCases.GetStockPrice;
-using TradingChat.Domain.Shared;
-
-namespace TradingChat.Application.Contracts;
-
-public interface IStockPriceService
-{
-    Task<Result<StockDataDto>> GetStockPrice(string stockCode);
-}
