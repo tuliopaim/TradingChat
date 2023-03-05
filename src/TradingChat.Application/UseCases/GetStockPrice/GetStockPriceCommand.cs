6fc39087be811fe0753d238b14f0dad09e184bf2
Túlio Paim <tutpaim@gmail.com>
2023-03-05 12:18:23 -0300

    feat: get stock price command

---
+++ src/TradingChat.Application/UseCases/GetStockPrice/GetStockPriceCommand.cs
@@ -0,0 +1,5 @@
+﻿using TradingChat.Application.Abstractions;
+
+namespace TradingChat.Application.UseCases.GetStockPrice;
+
+public record GetStockPriceCommand(string StockCode) : ICommand<StockDataDto>;
---
+++ src/TradingChat.Application/UseCases/GetStockPrice/GetStockPriceCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace TradingChat.Application.UseCases.GetStockPrice;
+
+public class GetStockPriceCommandValidator : AbstractValidator<GetStockPriceCommand>
+{
+    public GetStockPriceCommandValidator()
+    {
+        RuleFor(x => x.StockCode).NotEmpty();
+    }
+}
