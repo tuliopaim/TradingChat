a3874562abd65f9df99645891dfa4b9e9aebbc19
Túlio Paim <tutpaim@gmail.com>
2023-03-05 12:18:23 -0300

    feat: get stock price command

---
+++ src/TradingChat.Application/UseCases/GetStockPrice/StockDataDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TradingChat.Application.UseCases.GetStockPrice;
+
+public record StockDataDto(
+    string Symbol,
+    DateOnly Date,
+    TimeOnly Time,
+    decimal Open,
+    decimal High,
+    decimal Low,
+    decimal Close,
+    decimal Volume);
