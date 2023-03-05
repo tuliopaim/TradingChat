b8176c84c8a5ebbebddc443486c0783b5a56e93c
Túlio Paim <tutpaim@gmail.com>
2023-03-05 12:18:01 -0300

    feat: stooq infrastructure project with tests

---
+++ src/TradingChat.Application/Contracts/IStockPriceService.cs
@@ -0,0 +1,9 @@
+﻿using TradingChat.Application.UseCases.GetStockPrice;
+using TradingChat.Domain.Shared;
+
+namespace TradingChat.Application.Contracts;
+
+public interface IStockPriceService
+{
+    Task<Result<StockDataDto>> GetStockPrice(string stockCode);
+}
