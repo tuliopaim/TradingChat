b9beeba25b238ea894eb97c54b9f43f5fe3966fd
Túlio Paim <tutpaim@gmail.com>
2023-03-05 10:51:49 -0300

    feat: implement the GetChatMessagesQuery with the view

---
+++ src/TradingChat.Application/UseCases/GetChatMessages/GetChatMessagesQuery.cs
@@ -0,0 +1,9 @@
+﻿using TradingChat.Application.Abstractions;
+using TradingChat.Application.UseCases.Shared;
+
+namespace TradingChat.Application.UseCases.GetChatMessages;
+
+public record GetChatMessagesQuery(Guid ChatRoomId) : IQuery<ChatMessagesDto>;
+    
+
+
---
+++ src/TradingChat.Application/UseCases/Shared/ChatMessagesDto.cs
@@ -0,0 +1,20 @@
+﻿namespace TradingChat.Application.UseCases.Shared;
+
+public class ChatMessagesDto
+{
+    public required Guid ChatRoomId { get; init; }
+    public required string Name { get; init; }
+    public IEnumerable<ChatMessageInfoDto> Messages { get; set; } = new List<ChatMessageInfoDto>();
+}
+
+public class ChatMessageInfoDto
+{
+    public required Guid Id { get; init; }
+    public required string Message { get; init; }
+    public required DateTimeOffset SentAt { get; init; }
+    public required string User { get; init; }
+
+    public string SentAtString => SentAt
+        .LocalDateTime
+        .ToString("dd/MM/yyyy HH:mm");
+}
