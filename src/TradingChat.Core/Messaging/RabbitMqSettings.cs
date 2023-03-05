afa5bdf57f4c1ecbf30024de739014179c11d61c
Túlio Paim <tutpaim@gmail.com>
2023-03-05 14:38:26 -0300

    feat: core project with rabbitmq config

---
+++ src/TradingChat.Core/Messaging/RabbitMqSettings.cs
@@ -0,0 +1,16 @@
+namespace TradingChat.Core.Messaging;
+
+public class RabbitMqSettings
+{
+    public required string HostName { get; set; }
+    public required string UserName { get; set; }
+    public required int Port { get; set; }
+    public required string Password { get; set; }
+    public required RabbitMqRetrySettings RetrySettings { get; set; }
+}
+
+public class RabbitMqRetrySettings
+{
+    public required int Count { get; set; }
+    public required int DurationInSeconds { get; set; }
+}
---
+++ src/TradingChat.Core/Messaging/RabbitRoutingKeys.cs
@@ -0,0 +1,13 @@
+﻿namespace TradingChat.Core.Messaging;
+
+public class RabbitRoutingKeys
+{
+    public const string ChatCommand = "ChatCommand";
+    public const string ChatMessage = "ChatMessage";
+
+    public static string[] All => new[]
+    {
+        ChatCommand,
+        ChatMessage,
+    };
+}
