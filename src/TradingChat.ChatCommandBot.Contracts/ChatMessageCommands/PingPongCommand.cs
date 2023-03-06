b7f632deabab6d39dcb58a8298aa7de4b77a714a
Túlio Paim <tutpaim@gmail.com>
2023-03-05 21:05:31 -0300

    feat: implement ping pong chat message command

---
+++ src/TradingChat.ChatCommandBot.Contracts/ChatMessageCommands/PingPongCommand.cs
@@ -0,0 +1,39 @@
+﻿using TradingChat.ChatCommandBot.Commands.Contracts;
+using TradingChat.Core;
+using TradingChat.Core.Messages;
+using TradingChat.Core.Messaging;
+
+namespace TradingChat.ChatCommandBot.Commands.ChatMessageCommands;
+
+public class PingPongCommand : IChatMessageCommand
+{
+    private readonly IMessageProducer _messageProducer;
+
+    public PingPongCommand(IMessageProducer messageProducer)
+    {
+        _messageProducer = messageProducer;
+    }
+
+    private const string _pingCommand = "/ping";
+    private const string _responseMessage = "pong";
+    private const string _responseMessageCapital = "Pong";
+
+    public bool CanExecute(string message)
+    {
+        return message
+            .Equals(_pingCommand, StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    public Task<Result> ExecuteAsync(string message, Guid chatId)
+    {
+        _messageProducer.Publish(new NewMessage
+        {
+            Message = char.IsUpper(message[1])
+                ? _responseMessageCapital
+                : _responseMessage,
+            ChatRoomId = chatId
+        }, QueueNames.ChatMessage);
+
+        return Task.FromResult(Result.Success());
+    }
+}
